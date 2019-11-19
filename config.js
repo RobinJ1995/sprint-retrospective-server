659eb1eb5984734c414c2a4b488dd8d4d46e4315
Robin Jacobs <robin@robinj.be>
2019-11-19 21:52:22 +0000

    Allow supplying a connection string for mongodb

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ module.exports = config = {
         port: process.env.DB_PORT || 27017,
         name: process.env.DB_NAME || 'sprint-retrospective',
         username: process.env.DB_USERNAME || 'root',
-        password: process.env.DB_PASSWORD || 'root'
+        password: process.env.DB_PASSWORD || 'root',
+        connectionString: process.env.DB_CONNECTION_STRING
     }
 };
