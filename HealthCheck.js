1b1d203dfb8715c0f4089d2ee74f31694c0880ad
Robin Jacobs <robin@robinj.be>
2020-07-03 12:57:42 +0200

    Failed health check logging, close message queue channels

--- HealthCheck.js
+++ HealthCheck.js
@@ -6,10 +6,34 @@ module.exports = class HealthCheck {
 	}
 
 	run = () => Promise.all([
-		this.req.database.stats().then(({ ok }) => !!ok).catch(() => false),
-		this.req.redis.infoAsync().then(x => x.includes('redis_version')).catch(() => false),
-		messageQueue.send('HEALTH').then(() => true).catch(() => false)
+		this._database(),
+		this._redis(),
+		this._message_queue()
 	]).then(([ database, redis, message_queue ]) => ({
 		database, redis, message_queue
 	}));
+
+	_database = () => this.req.database.stats()
+		.then(({ ok }) => !!ok)
+		.catch(err => {
+			console.log('Database health check failed.', err);
+
+			return false;
+		});
+
+	_redis = () => this.req.redis.infoAsync()
+		.then(x => x.includes('redis_version'))
+		.catch(err => {
+			console.log('Redis health check failed.', err);
+
+			return false;
+		});
+
+	_message_queue = () => messageQueue.send('HEALTH')
+		.then(() => true)
+		.catch(err => {
+			console.log('Message queue health check failed.', err);
+
+			return false;
+		});
 };
